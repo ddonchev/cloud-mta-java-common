c9ac033ad0f83e6e460520ecdbbc655cf0d0f620
Dimitar Donchev <dimitar.donchev@sap.com>
2019-03-15 18:44:14 +0200

    VersionRule.value(caseInsensitive)

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/VersionRule.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/VersionRule.java
@@ -37,4 +37,7 @@ public enum VersionRule {
         return versionRuleValidator.allows(deploymentType);
     }
 
+    VersionRule value(String caseInsensitiveValue) {
+        return VersionRule.valueOf(caseInsensitiveValue.toUpperCase());
+    }
 }
